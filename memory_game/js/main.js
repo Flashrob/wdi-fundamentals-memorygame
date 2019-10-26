3cead57a42e818fdbca6b1da88fa7e421fa17782
Rob <kolsek.income@gmail.com>
2019-10-27 02:16:31 +0800

    changed if statement

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,15 +2,16 @@ let cards = ["queen", "queen", "king", "king"]
 let cardsInPlay = []
 
 let cardOne = cards[0]
-cardsInPlay.push(cardOne)
 let cardTwo = cards[2]
+cardsInPlay.push(cardOne)
 cardsInPlay.push(cardTwo)
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!")
-} else {
-    alert("Sorry, try again.")
-}
+cardsInPlay[0] === cardsInPlay[1] ? alert("You found a match!") : alert("Sorry, try again.")
+// if (cardsInPlay[0] === cardsInPlay[1]) {
+//     alert("You found a match!")
+// } else {
+//     alert("Sorry, try again.")
+// }
