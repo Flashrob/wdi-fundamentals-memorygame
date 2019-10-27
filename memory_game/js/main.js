1370fc7334ed86270bdad6085265d061156e3635
Rob <kolsek.income@gmail.com>
2019-10-27 23:35:33 +0800

    created card objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,44 @@
-let cards = ["queen", "queen", "king", "king"]
+let cards = [{
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    }, {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+]
+
 let cardsInPlay = []
 
-function checkForMatch(){
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
-      } else {
-        console.log("Sorry, try again.");
-      }
-}
-
-function flipCard(cardId){
-    cardsInPlay.push(cards[cardId])
-    console.log("User flipped " + cards[cardId]);
-
+function checkForMatch() {
     if (cardsInPlay.length === 2) {
-        checkForMatch()
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert("You found a match!");
+        } else {
+            alert("Sorry, try again.");
+        }
     }
 }
+
+function flipCard(cardId) {
+    cardsInPlay.push(cards[cardId].rank)
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage)
+    console.log(cards[cardId].suit)
+    checkForMatch()
+}
+
 flipCard(0)
 flipCard(2)
-// supposedly should call checkForMatch() within flipCard, but then it checks even with only one flipped card. So I used an if function to have 2 cards.
+// supposedly should call checkForMatch() within flipCard, but then it checks even with only one flipped card. So I used an if function to have 2 cards in checkformatch
