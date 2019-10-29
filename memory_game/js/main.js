95c14407c62b806679ed5a06526e15f98f9af009
Rob <kolsek.income@gmail.com>
2019-10-29 14:49:51 +0800

    final version with score and reset button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,4 @@
+//cards array
 let cards = [{
         rank: "queen",
         suit: "hearts",
@@ -19,26 +20,69 @@ let cards = [{
     }
 ]
 
+//score selector for use in checkForMatch and flipCard
+let score = document.createElement("p")
+
+//cardsInPlay array and score counter
 let cardsInPlay = []
+let scoreCount = 0
 
 function checkForMatch() {
-    // supposedly should call checkForMatch() within flipCard, but then it checks, even with only one flipped card. So I used an if function to have 2 cards in checkformatch
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");   
+        scoreCount += 1  
+        score.textContent = scoreCount  
+    } else {
+        alert("Sorry, try again.");
+    }
+}
+
+function flipCard() {
+    //get index of clicked img, push suit into Array, set img to cardImage
+    let cardId = this.getAttribute("data-id")
+    cardsInPlay.push(cards[cardId].rank)
+    this.setAttribute("src", cards[cardId].cardImage)
+
+    // console.log("User flipped " + cards[cardId].rank);
+    // console.log(cards[cardId].cardImage)
+    // console.log(cards[cardId].suit)
+
     if (cardsInPlay.length === 2) {
-        if (cardsInPlay[0] === cardsInPlay[1]) {
-            alert("You found a match!");
-        } else {
-            alert("Sorry, try again.");
+        //if reset button doesnt exist, create one
+        if (!document.querySelector("button")) {
+            let reset = document.createElement("button")
+            reset.textContent = "Reset"
+            //reset each cardImage to card back and remove reset button on click
+            reset.addEventListener("click", function(){
+                let cardElement = document.querySelectorAll("img")
+                cardElement.forEach(function(card){
+                    card.setAttribute("src", "images/back.png")
+                    reset.remove()
+                })
+            })
+            document.querySelector("main").appendChild(reset)
+        }
+        checkForMatch()
+        cardsInPlay = []
+        //creating score counter underneath instructions
+        if (!document.querySelector("#score")) {
+            score.textContent = `Score: ${scoreCount}`
+            score.setAttribute("id", "score")
+            let gameBoard = document.querySelector("#game-board")
+            gameBoard.before(score)
         }
     }
 }
 
-function flipCard(cardId) {
-    cardsInPlay.push(cards[cardId].rank)
-    console.log("User flipped " + cards[cardId].rank);
-    console.log(cards[cardId].cardImage)
-    console.log(cards[cardId].suit)
-    checkForMatch()
+function createBoard() {
+    //iterate cards array. for each card, create one img on game board
+    for (let i = 0; i < cards.length; i++) {
+        let cardElement = document.createElement('img')
+        cardElement.setAttribute("src", "images/back.png")
+        cardElement.setAttribute("data-id", i)
+        document.getElementById("game-board").appendChild(cardElement)
+        cardElement.addEventListener("click", flipCard)
+    }
 }
 
-flipCard(0)
-flipCard(2)
+createBoard()
